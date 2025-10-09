05ed046d339207e79add5c7619aeaf527a842bc1
Ángel González De la Calle <gonzalezdelacalleangel@gmail.com>
2025-10-09 13:03:11 +0200

    Agregar todos los archivos

---
+++ src/main/java/com/trinitarias/gestorcromos/GestorcromosApplication.java
@@ -0,0 +1,13 @@
+package com.trinitarias.gestorcromos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorcromosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorcromosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/trinitarias/gestorcromos/GestorcromosApplicationTests.java
@@ -0,0 +1,13 @@
+package com.trinitarias.gestorcromos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorcromosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
