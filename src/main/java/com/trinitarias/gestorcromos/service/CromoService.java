26f06903ae5078bad6c986481cfea48753c14565
Ángel González De la Calle <gonzalezdelacalleangel@gmail.com>
2025-10-12 19:12:19 +0200

    Versión final: API RESTful Gestor de Cromos

--- src/main/java/com/trinitarias/gestorcromos/service/CromoService.java
+++ src/main/java/com/trinitarias/gestorcromos/service/CromoService.java
@@ -4,8 +4,6 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;   // marks this class as part of the service (business logic) layer
 
 import com.trinitarias.gestorcromos.Cromo;
